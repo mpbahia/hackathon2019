8f710c38e209139ee5df32b5757177a27263ef83
JimmyStarling <jimmystarlingdev@gmail.com>
2019-11-11 01:25:39 -0300

    Added Provider store

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'redux-starter-kit/redux' 
+import { Provider } from 'react-redux' 
 import './index.css'
 import App from './components'
 import * as serviceWorker from './serviceWorker'
