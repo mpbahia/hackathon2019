fe59a26a840ac5f3c66dc3519fd36330b32af581
JimmyStarling <jimmystarlingdev@gmail.com>
2019-11-12 02:10:49 -0300

    Added drawer styled component material ui

--- src/components/containers/Home/HomeContainer.jsx
+++ src/components/containers/Home/HomeContainer.jsx
@@ -1,19 +1,69 @@
 import React, { Component } from 'react'
-import { Grid } from '@material-ui/core';
-import ProductListContainer from './components/ProductList/ProductListContainer';
+import { Container, Divider,
+         List, ListItemText, ListItem,
+         Drawer } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ProductTableContainer from './components/ProductList/ProductTableContainer';
 import styled from 'styled-components'
 
-const Container = styled(Grid)`
+const themeMixin = {
+    sectiontheme: makeStyles(theme => theme.mixins.toolbar)
+}
+
+const AppDrawer = styled(Drawer)`
+    & .MuiDrawer-paper{
+        width: 300px;
+        flex-shrink: 0;
+    }
+`
+
+const ContentContainer = styled(Container)`
     width: 100%;
     height: 100%;
+    padding: 50px;
+`
+const Toolbar = styled.div`
+    ${props => styled(themeMixin.sectiontheme)}   
+`
+const Content = styled.main`
+    
 `
 
 class HomeContainer extends Component {
     render() {
         return (
-            <Container container spacing={3}>
-                <ProductListContainer/>
-            </Container>
+            <ContentContainer>
+                <AppDrawer
+                    className="drawer-home"
+                    variant="permanent"
+                    classes={{
+                        paper: "drawer-paper",
+                    }}
+                    anchor="left">
+                    <div className="toolbar-home" />
+                    <Divider />
+                    <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                    </List>
+                    <Divider />
+                    <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                    </List>
+                </AppDrawer>
+                <Content className={"content"}>
+                    <Toolbar className={"toolbar"}>
+                        <ProductTableContainer/>
+                    </Toolbar>
+                </Content>
+            </ContentContainer>
         )
     }
 }
